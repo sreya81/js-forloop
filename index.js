ed737c4a02ffc829e00e5803e8b7c58e47960eaa
sreya81 <sreyakachakayala06@gmail.com>
2024-11-27 01:59:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+//Q-1  print the numbers from 1 to 20
+for(var i=1;i<=20;i++){
+console.log(i);
+}
+
+console.log("...................................................");
+
+
+//Q-2 print the squares of numbers from 1 to 10
+for(var i=1;i<=10;i++){
+console.log(i*i);
+}
+
+
+
+
+console.log("......................................................");
+
+
+//Q-3 print the first 10 multiples of 5
+for(var i=1;i<=10;i++){
+console.log(i*5);
+}
+
+
+
+
+console.log("........................................................");
+
+
+//Q-4 print numbers from 20 to 1 in reverse order
+for(var i=20;i>0;i--){
+    console.log(i);
+}
+
+
+
+
+console.log(".........................................................");
+
+
+//Q-5 print the first 10 even numbers in reverse order
+for(var i=10;i>0;i--){
+    console.log(i*2);
+}
+       //  (or) 
+
+for(var i=20;i>0;i-=2){
+    console.log(i);
+}   
+
+
+
+
+
+console.log("........................................................");
+
+
+//Q-6 print each element of the array
+var arr=[10,20,30,40,50];
+for(i=0;i<arr.length;i++){   
+console.log(arr[i]);
+}
+
+
+
+
+
+
+console.log(".........................................................");
+
+
+
+// Q-7 print the index of each element along with its value
+var array=['apple','banana','cherry'];
+for(j=0;j<array.length;j++){
+    // console.log(array,typeof array);
+    console.log('index',j,':',array[j]);
+}
+
+
